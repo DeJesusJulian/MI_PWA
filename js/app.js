a63639ed4c42b5aaf73cae054beab4cec106cfe2
DeJesusJulian <luis.rey575@gmail.com>
2021-12-04 22:11:34 -0600

    change 2

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ if (navigator.serviceWorker) {
     if (url.includes('localhost')) {
         swDirect = "/sw.js";
     }
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register(swDirect)
 } else {
     console.log('Lastima cambia de navegador');
 }
